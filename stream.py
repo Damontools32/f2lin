4213270987ff4444772ceff06c54b58460ee7197
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-07-15 00:22:23 +0330

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,16 +1,39 @@
-from telethon import TelegramClient, events
+import os
+from telethon.sync import TelegramClient, events
+from instaloader import Instaloader, Post, Profile
 
-api_id = 'YOUR_API_ID'  # جایگزینی با شناسه API خود
-api_hash = 'YOUR_API_HASH'  # جایگزینی با هش API خود
-bot_token = 'YOUR_BOT_TOKEN'  # جایگزینی با توکن ربات خود
+API_ID = 'your_telegram_api_id'  # replace with your API ID
+API_HASH = 'your_telegram_api_hash'  # replace with your API Hash
+BOT_TOKEN = 'your_bot_token'  # replace with your Bot Token
 
-bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+# create a instaloader instance
+L = Instaloader()
 
-@bot.on(events.NewMessage)
-async def handler(event):
-    if event.message.file:
-        from_user = event.message.sender_id
-        await bot.forward_messages(from_user, event.message, event.chat_id)
+# create a telegram client instance
+with TelegramClient('anon', API_ID, API_HASH).start(bot_token=BOT_TOKEN) as client:
 
-with bot:
-    bot.run_until_disconnected()￼Enter
+    @client.on(events.NewMessage)
+    async def my_event_handler(event):
+        if 'instagram.com' in event.raw_text:
+            url = event.raw_text
+            is_story = 'stories' in url
+
+            if is_story:
+                username, story_id = url.split('/')[-3], url.split('/')[-2]
+                profile = Profile.from_username(L.context, username)
+
+                for story in L.get_stories(userids=[profile.userid]):
+                    for item in story.get_items():
+                        if str(item.mediaid) == story_id and item.is_video:
+                            video_url = item.video_url
+                            await client.send_file(event.chat_id, video_url)
+                            break
+            else:
+                shortcode = url.split('/')[-2]
+                post = Post.from_shortcode(L.context, shortcode)
+
+                if post.is_video:
+                    video_url = post.video_url
+                    await client.send_file(event.chat_id, video_url)
+
+    client.run_until_disconnected()
